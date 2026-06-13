e0e011780383ab54bb35e43430f048ecd54d95e6
Divya Bandi <divyabandi@Divyas-MacBook-Neo.local>
2026-06-14 00:10:12 +0530

    Add unit tests project

--- Program.cs
+++ Program.cs
@@ -4,14 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IConversionService, ConversionService>();
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
